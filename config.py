d01c98327902bf8c8b983da7bce7c77edc7b6c17
Centauria <centauriavc@gmail.com>
2020-07-09 02:00:15 +0800

    optimize

--- config.py
+++ config.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-configs_SC_Single_Speaker_ivectors = {"input_dim": 40,
-                                      "average_pooling": 301,
-                                      "cnn_configs": [[2, 64, 3, 1], [64, 64, 3, 1], [64, 128, 3, (2, 1)],
-                                                      [128, 128, 3, 1]],
-                                      "speaker_embedding_dim": 100,
-                                      "splice_size": 20 * 128 + 100,
-                                      "Linear_dim": 384,
-                                      "Shared_BLSTM_dim": 896,
-                                      "Linear_Shared_layer1_dim": 160,
-                                      "Linear_Shared_layer2_dim": 160,
-                                      "BLSTM_dim": 896,
-                                      "BLSTM_Projection_dim": 160,
-                                      "output_dim": 3,
-                                      "output_speaker": 1
-                                      }
+configs_SC_Single_Speaker_ivectors = {
+    "input_dim": 40,
+    "average_pooling": 301,
+    "cnn_configs": [
+        [2, 64, 3, 1],
+        [64, 64, 3, 1],
+        [64, 128, 3, (2, 1)],
+        [128, 128, 3, 1]
+    ],
+    "speaker_embedding_dim": 100,
+    "splice_size": 20 * 128 + 100,
+    "Linear_dim": 384,
+    "Shared_BLSTM_dim": 896,
+    "Linear_Shared_layer1_dim": 160,
+    "Linear_Shared_layer2_dim": 160,
+    "BLSTM_dim": 896,
+    "BLSTM_Projection_dim": 160,
+    "output_dim": 3,
+    "output_speaker": 1
+}
